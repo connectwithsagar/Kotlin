ce6e367178b3f811db0efc79e7f345f4423c0721
Sagar Singh <whitesangjr07@gmail.com>
2024-08-25 23:42:41 +0530

    second commit

--- Arrayhai.kt
+++ Arrayhai.kt
@@ -14,15 +14,27 @@ fun main(){
     val words: Array<String> = arrayOf("kotlin","java","python")
     println(words[2])
 
+    //change in array
+    words[1] = "PhP"
+
+    //Array Size
+    println(words.size)
+
+    //Compare arrays(it checks two arrays have the same element in the same order
+    println(numbers.contentEquals(words))
+
+    //To shuffle elements in the array
+    words.shuffle()
+
 //    type 3 to initialise an Array
-    val squares =Array(5){i -> i*i}
-    val size = squares.size
-    println(size)
-    if(squares.isEmpty()){
-        println("Array is empty")
-    }else{
-        println("Array is not empty")
-    }
+//    val squares =Array(5){i -> i*i}
+//    val size = squares.size
+//    println(size)
+//    if(squares.isEmpty()){
+//        println("Array is empty")
+//    }else{
+//        println("Array is not empty")
+//    }
 
 //    val matrix: Array<Array<Int>> = arrayOf(1,2,3, arrayOf(4,5,6, arrayOf(7,8,9)))
 
