5f7b2abb880b6b932de819d6c3ae7511f0743d87
Sagar Singh <whitesangjr07@gmail.com>
2024-08-27 17:16:59 +0530

    fourth commit

--- Arrayhai.kt
+++ Arrayhai.kt
@@ -1,44 +1,44 @@
-fun main(){
+//fun main(){
 
 //    type 1 to initialise an Array
-    val numbers = arrayOf(1,2,3,4,5)
-
-    println(numbers[0])
-    println(numbers[1])
-    println(numbers[2])
-    println(numbers[3])
-    println(numbers[4])
+//    val numbers = arrayOf(1,2,3,4,5)
+//
+//    println(numbers[0])
+//    println(numbers[1])
+//    println(numbers[2])
+//    println(numbers[3])
+//    println(numbers[4])
 
 //    type 2 to initialise an Array
 
-    val words: Array<String> = arrayOf("kotlin","java","python")
-    println(words[2])
-
-    //change in array
-    words[1] = "PhP"
-
-    //Array Size
-    println(words.size)
-
-    //Compare arrays(it checks two arrays have the same element in the same order
-    println(numbers.contentEquals(words))
-
-    //To shuffle elements in the array
-    words.shuffle()
+//    val words: Array<String> = arrayOf("kotlin","java","python")
+//    println(words[2])
+//
+//    //change in array
+//    words[1] = "PhP"
+//
+//    //Array Size
+//    println(words.size)
+//
+//    //Compare arrays(it checks two arrays have the same element in the same order
+//    println(numbers.contentEquals(words))
+//
+//    //To shuffle elements in the array
+//    words.shuffle()
 
 //    type 3 to initialise an Array
-    val squares =Array(5){i -> i*i}
-    val size = squares.size
-    println(size)
-    if(squares.isEmpty()){
-        println("Array is empty")
-    }else{
-        println("Array is not empty")
-    }
+//    val squares =Array(5){i -> i*i}
+//    val size = squares.size
+//    println(size)
+//    if(squares.isEmpty()){
+//        println("Array is empty")
+//    }else{
+//        println("Array is not empty")
+//    }
 
 //    val matrix: Array<Array<Int>> = arrayOf(1,2,3, arrayOf(4,5,6, arrayOf(7,8,9)))
 
-}
+//}
 //fun main(args:Array<String>){
 //    var arr1= arrayOf(1,10,4,6,15)
 //    println(arr1.lastIndex)
@@ -58,3 +58,22 @@ fun main(){
 //    println(arr1.sorted())
 //    println(arr1)
 //    println(arr1.minOrNull())
+
+fun main() {
+    val numbers = arrayOf(1,2,3,4,5,6,7,8,9,10,12,)
+    var oddsum = 0
+    var evensum = 0
+
+
+    for (number in numbers) {
+        if (number % 2 == 0) {
+            evensum += number
+        } else {
+            oddsum += number
+        }
+    }
+
+
+    println("Sum of even numbers: $evensum")
+    println("Sum of odd numbers: $oddsum")
+}
---
+++ Classes.kt
@@ -0,0 +1,53 @@
+//class Subway {
+//    var name: String = "Po"
+//    val age:Int=22
+//    fun displayInfo():String {
+//        println("My name is $name and my age is $age")
+//    }
+//    fun message(){
+//        println("Good Morning, sunshine!")
+//    }
+//
+//}
+//fun main(){
+//    val person = Subway()
+//    person.displayInfo()
+//    person.message()
+//}
+
+//val x = anytask();
+fun anytask(num1: Int, num2 :Int):Int{
+//    var a:Int
+//    var b:Int
+//    val c = num1 +num2
+//    println(c)
+//    return c
+//}
+//
+//var count = 0
+//fun rec(){
+//    count++;
+//    if(count <= 5){
+//        println("hello "+count);
+//        rec();
+//    }
+//}
+//fun main(){
+////    val result = anytask()
+////    println(result)
+//    val x = anytask(4,2)
+//    val y = rec()
+//}
+
+    class car{
+    var make : String =""
+        var model : String = ""
+        var year : Int = 0
+        fun getInfo(){
+        return "$make $model, year $year"}
+    }
+}
+class Car1 {
+    var make: String = ""
+    var maodel : String =""
+}
